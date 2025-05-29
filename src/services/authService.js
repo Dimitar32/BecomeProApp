e39e196cc7a775505011e9ee74dfbf5f1eeed8da
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2025-05-29 23:03:42 +0300

    add api

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = 'https://becomeproappservice.onrender.com';//process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 // логин: връща { token, user }
 export const login = async (userName, password) => {
--- src/services/trainingService.js
+++ src/services/trainingService.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://becomeproappservice.onrender.com/api';//process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
 // Get all training categories
 export async function getTrainingCategories(token) {
