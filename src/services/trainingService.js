7fd6417a7a67b6e2e7e67525347ca8b60e78c526
Dimitar Ivanov <dimitarivanov2525@gmail.com>
2026-01-08 16:42:43 +0200

    get back to api url hardcode

--- src/services/trainingService.js
+++ src/services/trainingService.js
@@ -1,8 +1,8 @@
 import axios from 'axios';
 
-// const API_URL = 'https://becomeproappservice.onrender.com/api';//process.env.REACT_APP_API_URL || 'http://localhost:5000';
+const API_URL = 'https://becomeproappservice.onrender.com/api';//process.env.REACT_APP_API_URL || 'http://localhost:5000';
 
-const API_URL = process.env.REACT_APP_API_URL;
+// const API_URL = process.env.REACT_APP_API_URL;
 
 // Get all training categories
 export async function getTrainingCategories(token) {
